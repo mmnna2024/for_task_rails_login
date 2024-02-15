17e5d796224f2034da6ffdcc25c0ace9c6d1f7d2
naomu <muramatsu2023@gmail.com>
2024-02-15 18:10:40 +0900

    first commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+    skip_before_action :login_required, only: [:new, :create]
+    
+    def new
+    end
+
+    def create
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user&.authenticate(params[:session][:password])
+            log_in(user)
+            redirect_to tasks_path, flash: {success: 'ログインしました'}
+        else
+            flash.now[:danger] = 'メールアドレスまたはパスワードに誤りがあります'
+            render :new
+        end
+    end
+
+    def destroy
+        session.delete(:user_id)
+        flash[:notice] = 'ログアウトしました'
+        redirect_to new_session_path
+    end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+    validates :name, presence: true, length: { maximum: 30 }
+    validates :email, presence: true, length: { maximum: 255 }, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :password, length: { minimum: 6 }
+    has_secure_password
+end
