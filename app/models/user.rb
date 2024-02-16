475d533f60f0c94149a88f8d3fc44a26df1f71fc
naomu <muramatsu2023@gmail.com>
2024-02-16 11:59:09 +0900

    8th commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_many :tasks
     validates :name, presence: true, length: { maximum: 30 }
-    validates :email, presence: true, length: { maximum: 255 }, uniqueness: { message: 'は使用されています' }
-    validates :password, length: { minimum: 6 }
+    validates :email, presence: true, length: { maximum: 255 }, uniqueness: true
+    validates :password, presence: true, length: { minimum: 6 }
     has_secure_password
 end
